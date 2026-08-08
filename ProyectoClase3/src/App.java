f03fb18ce31fa8f342f43c9c7ea56f0537843d53
IanFig <ifigueroa@cdainfo.com>
2026-08-08 15:38:30 -0300

    ejercicio 3 - flujo agregado

--- ProyectoClase3/src/App.java
+++ ProyectoClase3/src/App.java
@@ -2,4 +2,15 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Nuevo proyecto clase 3");
     }
+
+    /*
+    Flujo:
+        Producto    -> Cuenta bancaria
+                                        -> Caja de ahorro
+                                        -> Cuenta Corriente
+                    -> Plazo Fijo
+                    -> Tarjeta de Credito
+
+
+    */
 }
--- ProyectoClase3/src/models/TarjetaCredito.java
+++ ProyectoClase3/src/models/TarjetaCredito.java
@@ -4,6 +4,12 @@ public class TarjetaCredito extends Producto{
     private double limiteCredito;
 
     public void pagarConTarjeta(double monto){
-        System.out.println("");
+        limiteCredito -= super.saldo;
+        System.out.println("Se bajó el limite de la tarjeta, quedara: " + limiteCredito);
+    }
+
+    public void pagarTarjeta(double monto){
+        limiteCredito += super.saldo;
+        System.out.println("Se incrementó el limite de la tarjeta, quedara: " + limiteCredito);
     }
 }
